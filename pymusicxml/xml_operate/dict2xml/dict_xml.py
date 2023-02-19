41cdb0bd4b4f3c1c85c07eb94c650f1295c62da4
bgArray <474037765@qq.com>
2023-02-19 09:50:51 +0800

    update 2023/2/18 dict_to_xml update2.4.2

--- pymusicxml/xml_operate/dict2xml/dict_xml.py
+++ pymusicxml/xml_operate/dict2xml/dict_xml.py
@@ -42,13 +42,68 @@ test1 = {'score-partwise': {'@version': '4.0', 'work': {'work-number': 'D. 911',
 #             if is_end:
 #                 return key, data
 
-def walk(data: dict):
-    pass
+
+def walk(data_in: dict):
+    # print("dy")
+    copy_ = str(data_in.copy())
+    for i in data_in.keys():
+        # print(i)
+        data = data_in[i]  # 当前key所对应的value赋给data
+        if isinstance(data, list):
+            data = data[0]
+        # print(data)
+
+        result: str = ""
+
+        if str(data_in).count("{") == 1 and str(data_in).count("}") == 1:  # 最底层的key
+            print("f")
+            print(data)
+            copy_ = copy_.replace(copy_, str(data))
+            return result + str(data), copy_
+        else:
+            if isinstance(data, dict):
+                # print("g")
+                # result += f"<{i}>"
+                # result += walk(data)
+                # result += f"</{i}>"
+
+                # print("h")
+                # print(str(data) + "/////")
+                again = walk(data)
+                try:
+                    print("again: " + again[0])
+                    result += again
+                except:
+                    print("h")
+                    print(str(data) + "/////")
+                    print("again: " + str(again))
+
+                finally:
+                    result += result + str(data)
+                    copy_ = copy_.replace(copy_, str(data))
+    print("copy_: " + copy_)
+    return result, copy_
+
+            # if isinstance(data, dict):
+            #     result += f"<{i}>"
+            #     result += walk(data)
+            #     result += f"</{i}>"
+            # else:
+            #     if isinstance(data, list):
+            #         for j in data:
+            #             result += f"<{i}>"
+            #             result += walk(j)
+            #             result += f"</{i}>"
+            #     else:
+            #         result += f"<{i}>{data}</{i}>"
+
 
 
 if __name__ == '__main__':
     # xml_ = _dict_2_xml(test1)
     # print(xml_)
-    for i in walk(test1):
-        print(i)
+    # for i in walk(test1):
+    #     print(i)
     # import os
+    print("copy:" + walk(test1)[1])
+    print(test1)
