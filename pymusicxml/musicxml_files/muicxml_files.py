9e53af403eafc9ee65c3b41d8d759e875724bf47
bgArray <474037765@qq.com>
2023-02-22 19:32:04 +0800

    update 2023/2/22 xml document translate update1

--- pymusicxml/musicxml_files/muicxml_files.py
+++ pymusicxml/musicxml_files/muicxml_files.py
@@ -12,3 +12,11 @@ class MusicxmlFile(object):
             if isinstance(file, str):
                 file = pathlib.Path(file)
             self.file = file
+
+    def _parse(self):
+        pass
+
+
+if __name__ == '__main__':
+    import mido
+    f = mido.MidiFile("1.mid")
